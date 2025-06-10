e71385debe213d10f37cfe0270da3a385b30afda
Madhusudan Singh Chauhan <madhusudanchauhan62@gmail.com>
2025-06-10 10:02:39 +0530

    write

---
+++ PerimeterOfRectangle.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class PerimeterOfRectangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Write Length");
+        int l = sc.nextInt();
+        System.out.println("Write Width!");
+        int w = sc.nextInt();
+        int per = 2 * (l*w);
+        System.out.println(per);
+
+    }
+}
