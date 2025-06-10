e739862140e970555737e7d76b69f9222f51ccc1
Madhusudan Singh Chauhan <madhusudanchauhan62@gmail.com>
2025-06-10 09:58:47 +0530

    write

--- Simple_Interest.java
+++ Simple_Interest.java
@@ -1,4 +1,15 @@
-
+import java.util.Scanner;
 public class Simple_Interest {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Write Input!");
+        int principal = sc.nextInt();
+        int rate = sc.nextInt();
+        int time = sc.nextInt();
+        double SI = (principal*rate*time)/100;
+        System.out.println(SI);
+
+
+    }
     
 }
